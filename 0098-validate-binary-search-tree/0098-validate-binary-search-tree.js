561bd7444867cdb978552030fe370447775353c3
Justin Turkaly <jturkaly18@gmail.com>
2023-02-15 13:57:38 -0600

    Time: 77 ms (54.05%), Space: 46.8 MB (15.01%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
@@ -23,31 +23,31 @@ Pseudo -
 */
 var isValidBST = function(root) {
   
-  const recurse = (node, min, max) => {
-    if (node === null) return true;
+//   const recurse = (node, min, max) => {
+//     if (node === null) return true;
     
-    if (node.val >= max || node.val <= min) return false;
+//     if (node.val >= max || node.val <= min) return false;
     
-    return recurse(node.left, min, node.val) && recurse(node.right, node.val, max);
+//     return recurse(node.left, min, node.val) && recurse(node.right, node.val, max);
+//   }
+//   return recurse(root, -Infinity, Infinity)
+  let maybeSorted = [];
+  
+  const inOrderDfs = (node) => {
+    if (!node) return;
+    
+    inOrderDfs(node.left);
+    maybeSorted.push(node);
+    inOrderDfs(node.right);
   }
-  return recurse(root, -Infinity, Infinity)
-//   let maybeSorted = [];
+  inOrderDfs(root);
   
-//   const inOrderDfs = (node) => {
-//     if (!node) return;
+  for (let i = 0; i < maybeSorted.length - 1; i++) {
+    let current = maybeSorted[i];
+    let next = maybeSorted[i + 1];
     
-//     inOrderDfs(node.left);
-//     maybeSorted.push(node);
-//     inOrderDfs(node.right);
-//   }
-//   inOrderDfs(root);
+    if (current.val >= next.val) return false
+  }
   
-//   for (let i = 0; i < maybeSorted.length - 1; i++) {
-//     let current = maybeSorted[i];
-//     let next = maybeSorted[i + 1];
-    
-//     if (current.val >= next.val) return false
-//   }
-  
-//   return true;
+  return true;
 };
