84b1ad2f800ca6ee710687c21e3c138c56edd89d
Justin Turkaly <jturkaly18@gmail.com>
2023-03-24 10:34:06 -0500

    Time: 51 ms (93.19%), Space: 41.7 MB (60.26%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -6,8 +6,8 @@
 
 */
 var climbStairs = function(n) {
-    const memo = [1, 1, 2];
-  for(let i = 3; i <= n; i++) {
+  const memo = [1, 1];
+  for(let i = 2; i <= n; i++) {
     memo[i] = memo[i - 1] + memo[i - 2];
   }
   return memo[n]
