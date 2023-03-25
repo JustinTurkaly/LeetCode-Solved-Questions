e691ebdeac98e565b2602587cba37e407a2a7e3e
Justin Turkaly <jturkaly18@gmail.com>
2023-03-25 11:27:25 -0500

    Time: 68 ms (80.41%), Space: 46.3 MB (18.37%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.js
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.js
@@ -12,29 +12,32 @@
  */
 
 /*
-Input - the root of binary tree
-Output - the max diameter of binary tree,diameter of a binary tree is the length of the longest path between any two nodes in a tree
+Input - root of binary tree
+Output - length of longest path between any two nodes
 Constr/Edge - no empty inputs
-Pseudo - 
-1 - set a global max variable and define dfs function
-2 - base case of null child returns 0
-3 - recurse on both sides
-4 - reassign global max to the max of left plus right and old max
-5 - return the max of left and right incremented
+Plan - 
+- traverse tree
+  - starting bottom left count edges from parent then parents other children
+  - keep track of current edge max and global edge max
+- return global max
 */
 var diameterOfBinaryTree = function(root) {
-  let max = 0;
+  let maxEdge = 0;
   
   const dfs = (node) => {
-    if (!node) return 0;
+    if (!node) {
+      return 0;
+    }
     
     let left = dfs(node.left);
     let right = dfs(node.right);
+    let currentMax = left + right;
     
-    max = Math.max(left + right, max);
-    return Math.max(left, right) + 1;
+    maxEdge = Math.max(maxEdge, currentMax);
+    
+    return Math.max(left + 1, right + 1);
   }
-  
   dfs(root);
-  return max;
+  
+  return maxEdge;
 };
