5a27b44d924cd431258975953bf751d1df2a15e6
Justin Turkaly <jturkaly18@gmail.com>
2023-03-27 12:15:30 -0500

    Time: 88 ms (24.03%), Space: 45.9 MB (56.86%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -13,23 +13,15 @@ Plan -
 - reverse the second string and compare it to first string
 */
 var isPalindrome = function(s) {
-    // console.log(s.substring(0,10))
-    // console.log(s.substring(11).split('').reverse().join(''))
   temp = s.replace(/[^a-zA-Z0-9]/g, ''); // Remove all non-alphabetic characters
   formatted = temp.toLowerCase(); // Convert all alphabetic characters to lowercase
   console.log(formatted)
-  if (formatted.length % 2 !== 0) {
-    let mid = Math.floor(formatted.length / 2);
-    let firstHalf = formatted.substring(0, mid);
-    let secondHalf = formatted.substring(mid + 1);
-    let reversedHalf = secondHalf.split('').reverse().join('');
-    return firstHalf === reversedHalf;
-  } else {
-      let mid = Math.floor(formatted.length / 2);
-      let firstHalf = formatted.substring(0, mid);
-      let secondHalf = formatted.substring(mid);
-      let reversedHalf = secondHalf.split('').reverse().join('');
-      console.log(firstHalf, reversedHalf)
-      return firstHalf === reversedHalf;
-  }
+  let flag = formatted.length % 2 === 0;
+  
+  let mid = Math.floor(formatted.length / 2);
+  let firstHalf = formatted.substring(0, mid);
+  let secondHalf = flag ? formatted.substring(mid) : formatted.substring(mid + 1)
+  let reversedHalf = secondHalf.split('').reverse().join('');
+  return firstHalf === reversedHalf;
+  
 };
