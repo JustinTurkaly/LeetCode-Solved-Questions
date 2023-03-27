852a1b73964b73fb3a1e7e81bba85f81d9a61024
Justin Turkaly <jturkaly18@gmail.com>
2023-03-27 16:17:46 -0500

    Time: 71 ms (91.59%), Space: 42.6 MB (87.80%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -9,13 +9,24 @@ Input - two strings
 Output - a boolean representing if the strings are anagrams
 Constr/Edge - no empty inputs, only lowercase letters
 Plan - 
-- sort both arrays
-- return if they are equal
+- return false if lengths do not match
+- loop thru s and build freqMap
+- loop thru t 
+  - if current char is not present in freqMap return false
+    - if it is present, decrement count in map
+- return true
 */
 var isAnagram = function(s, t) {
-  return sort(s) === sort(t);   
+  if (s.length !== t.length) return false;
+  
+  const freqObj = s.split('').reduce((freq, char) => {
+    freq[char] = freq[char] ? freq[char] + 1 : 1;
+    return freq;
+  }, {});
+  
+  for (let i = 0; i < t.length; i++) {
+    if (!freqObj[t[i]]) return false;
+    freqObj[t[i]]--;
+  }
+  return true;
 };
-
-var sort = (str) => {
-  return str.split('').sort().join();
-}
