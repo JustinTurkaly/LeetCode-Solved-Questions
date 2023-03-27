323109d206aa7fdb0f0336db14dee621daf754e1
Justin Turkaly <jturkaly18@gmail.com>
2023-03-27 11:01:01 -0500

    Time: 106 ms (46.94%), Space: 50.1 MB (5.04%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+/*
+Input - array of ints and an integer
+Output - the indices of the two ints that add up to target
+Constr/Edge - guaranteed 2 ints in array,only one valid answer exists
+Plan - 
+- loop thru nums array
+  - check if the difference to target is inside indexObj
+  - if it is return [indexObj[difference], i]
+  - else add number to obj as a key and the index as the value
+*/
+var twoSum = function(nums, target) {
+  const indexObj = {};
+  for (let i = 0; i < nums.length; i++) {
+    let difference = target - nums[i];
+    console.log(indexObj[difference])
+    if (indexObj[difference] !== undefined) {
+      return [indexObj[difference], i];
+    } else {
+      indexObj[nums[i]] = i;
+    }
+  } 
+};
