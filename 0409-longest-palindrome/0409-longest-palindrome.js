e5c38262aa4c8ed6d011c425e511092659e64657
Justin Turkaly <jturkaly18@gmail.com>
2023-03-29 12:50:43 -0500

    Time: 209 ms (5.13%), Space: 50.3 MB (5.02%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.js
+++ 0409-longest-palindrome/0409-longest-palindrome.js
@@ -4,30 +4,34 @@
  */
 
 /*
-Input - string of upper and lowercase letters
-Output - the length of the longest palindrome that can be built with those letters
-Constr/Edge - no empty inputs, only upper/lowercase letters
+Input - a string
+Output - the length of the longest palindrome
 Plan - 
-- build a freq map out of the string
-- loop thru the values of that object
-  - if num is odd, increment count and flip boolean
-  - else add value to count
+- create a count var and a freqObj
+- loop thru string
+  - set n to 0
+  - if current char is inside freqObj
+    - set n = the value inside obj
+    - if n is even add to count
+  - if not inside
+    - assign char to key in obj with value of 1
 - return count
 */
 var longestPalindrome = function(s) {
   let count = 0;
-  let map = new Map();
+  let freqObj = {};
   
   for (let c of s) {
     let n = 0;
-    if (map.has(c)) {
-      n = map.get(c);
+    if (freqObj[c]) {
+      n = freqObj[c];
+      console.log(c, n)
       if (n % 2) {
         count += 2;
       }
     } 
-      map.set(c, n + 1);
+    freqObj[c] = n + 1;
+    
   }
-  
   return s.length > count ? count + 1 : count;
 };
